349f766ea8bc963d3508fb27de0f7f82b257b3e8
Vusumzi Mondi <vusumzimondi22@gmail.com>
2023-11-25 14:15:12 -0300

    courses home page not complete

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,17 @@
 class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+
+  end
+
+  def show
+    @course = Course.find(params[:id])
+
+  end
+
+  private
+  
+  def course_params
+    params.require(:course).permit(course_ids: [])
+  end
 end
