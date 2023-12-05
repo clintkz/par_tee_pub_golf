e4ca9190eeeca176834ec80f1a022865871fc27e
Clint Ziervogel <photon01.joins@icloud.com>
2023-12-05 20:06:57 +0200

    merging latest master

--- app/controllers/pub_scores_controller.rb
+++ app/controllers/pub_scores_controller.rb
@@ -1,3 +1,19 @@
 class PubScoresController < ApplicationController
-  
+  def score
+
+    @participant = Participant.find(params[:participant_id])
+    @game = Game.find(params[@participant.game_id])
+    @score = PubScore.New(score_params)
+    if @score.save
+      puts "your score is saved"
+    else
+      "Please enter your score before proceeding"
+    end
+  end
+
+
+
+
+  def score_params
+    params.require(:participant).permit(:game_id, :participant_id, :pub_id)
 end
--- app/models/pub_score.rb
+++ app/models/pub_score.rb
@@ -1,2 +1,3 @@
 class PubScore < ApplicationRecord
+  
 end
