4d1700eb3f11e5cfcd8e9a5e1b424d2f309c65ae
Guy Borthwick <guyborthwick@yahoo.com>
2023-12-05 21:35:16 +0200

    Added pub_score controller to save the score

--- app/controllers/pub_scores_controller.rb
+++ app/controllers/pub_scores_controller.rb
@@ -33,5 +33,22 @@ class PubScoresController < ApplicationController
     # params.require(:participant).permit(:game_id, :participant_id, :pub_id, :score)
     # params.permit(:game_id, :participant_id, :pub_id, :score)
     params.require(:pub_score).permit(:score, :pub_id)
+  def create
+    @pub_score = PubScore.new(pub_score_params)
+    if @pub_score.save
+      # Assuming @pub_score belongs to participant, and participant belongs to game
+      redirect_to participant_path(@pub_score.participant), notice: 'Score was successfully saved.'
+    else
+      # Handle the error case
+      # You might want to pass the ID of the game or participant to the 'new' template
+      # to ensure that the 'new' template can render correctly
+      redirect_to participant_path(@pub_score.participant), alert: 'Failed to save score.'
+    end
+  end
+
+  private
+
+  def pub_score_params
+    params.require(:pub_score).permit(:score, :pub_id, :participant_id)
   end
 end
