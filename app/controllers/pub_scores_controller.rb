fbfb1763827b58a929c1486854c4b8ed6acb9d7d
Guy Borthwick <guyborthwick@yahoo.com>
2023-12-09 12:13:16 +0200

    Pulling latest master

--- app/controllers/pub_scores_controller.rb
+++ app/controllers/pub_scores_controller.rb
@@ -36,12 +36,8 @@ class PubScoresController < ApplicationController
   def create
     @pub_score = PubScore.new(pub_score_params)
     if @pub_score.save
-      # Assuming @pub_score belongs to participant, and participant belongs to game
       redirect_to participant_path(@pub_score.participant), notice: 'Score was successfully saved.'
     else
-      # Handle the error case
-      # You might want to pass the ID of the game or participant to the 'new' template
-      # to ensure that the 'new' template can render correctly
       redirect_to participant_path(@pub_score.participant), alert: 'Failed to save score.'
     end
   end
