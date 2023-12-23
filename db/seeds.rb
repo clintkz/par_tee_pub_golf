c7b880b3e50726ca4748cb703f4ef49729a8e970
Guy Borthwick <guyborthwick@yahoo.com>
2023-12-23 09:45:51 +0200

    Added images to pubs in seed, removed padding from courses show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ User.destroy_all
 courses = {
   "South Ken Classic" => [
     { name: "Angelsea Arms", address: "15 Selwood Terrace, South Kensington, London SW7 3QG", image: "anglesea_arms.png", drink: "Lager", strokes: "3", rules: "If in one gulp your drink goes down, you get to nominate, wear a crown. Choose a friend to take a sip, watch them raise their glass, no need to flip!" },
-    { name: "Drayton Arms", address: "153 Old Brompton Rd, Greater, London SW5 0LJ", image: "beer.png", drink: "Gin and Tonic", strokes: "2", rules: "In our game, there's one more twist, no restroom breaks, don't get me amiss. Stay on the course, don't take a pit stop, or else, my friend, it's another drink to the top!" },
+    { name: "Drayton Arms", address: "153 Old Brompton Rd, Greater, London SW5 0LJ", image: "https://res.cloudinary.com/dis9yhfev/image/upload/v1703317379/image18-2_i7wbpu.jpg", drink: "Gin and Tonic", strokes: "2", rules: "In our game, there's one more twist, no restroom breaks, don't get me amiss. Stay on the course, don't take a pit stop, or else, my friend, it's another drink to the top!" },
     { name: "Nam Long", address: "159 Old Brompton Rd, London SW5 0LJ", image: "beer.png", drink: "Flaming Ferrari", strokes: "1", rules: "Keep your left hand on your drink, no switching, don't overthink. If you lift it with your right, take a shot, oh what a sight!" }
   ],
   "The Old Course"  => [
@@ -24,8 +24,8 @@ courses = {
   ],
   "James Blunt" => [
     { name: "The Fox and the Pheasant", address: "1 Billing Rd, London SW10 9UJ", image: "https://res.cloudinary.com/dis9yhfev/image/upload/v1703304196/FoxandPheasant_rd7ift.jpg", drink: "Guinness", strokes: "2", rules: "When I was young, at every tee, you must start each sentence, that's the decree, in a Scottish brogue, you must speak, no words astray. If you slip, take a wee sip, golf and accents, make it your way!"  },
-    { name: "Hollywood Arms", address: "45 Hollywood Rd, London SW10 9HX", image: "beer.png", drink: "Vodka Lime and Soda", strokes: "3", rules: "Declare a 'silent hole,' not a peep or you pay the toll. If you utter a word, it's a penalty drink, keep your lips sealed, or to the bar, you'll slink!" },
-    { name: "Maggies Club", address: "329 Fulham Rd, London SW10 9QL", image: "beer.png", drink: "Tequila", strokes: "1", rules: "On one leg, you must putt with glee, if you falter and lose your stance, add an extra shot to your score, you see!"  }
+    { name: "Hollywood Arms", address: "45 Hollywood Rd, London SW10 9HX", image: "https://res.cloudinary.com/dis9yhfev/image/upload/v1703316783/hollywood-arms_lnu967.webp", drink: "Vodka Lime and Soda", strokes: "3", rules: "Declare a 'silent hole,' not a peep or you pay the toll. If you utter a word, it's a penalty drink, keep your lips sealed, or to the bar, you'll slink!" },
+    { name: "Maggies Club", address: "329 Fulham Rd, London SW10 9QL", image: "https://res.cloudinary.com/dis9yhfev/image/upload/v1703316783/maggies_n4zyhi.jpg", drink: "Tequila", strokes: "1", rules: "On one leg, you must putt with glee, if you falter and lose your stance, add an extra shot to your score, you see!"  }
   ]
 }
 # Create Courses and Pubs
